79ddcbaba03cf25d13d0876b49ee25a2bbcd95ea
Mohammad <mhmehson@gmail.com>
2018-09-20 21:45:19 -0500

    springboot project created

---
+++ groodle-server/src/main/java/edu/uno/cs/csci6363/groodleserver/GroodleServerApplication.java
@@ -0,0 +1,12 @@
+package edu.uno.cs.csci6363.groodleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroodleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroodleServerApplication.class, args);
+	}
+}
