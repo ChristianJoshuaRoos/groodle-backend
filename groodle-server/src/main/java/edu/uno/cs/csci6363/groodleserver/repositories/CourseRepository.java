fd7a6096827002e58ee2a3450a69e7ecc4f3606e
Mohammad <mhmehson@gmail.com>
2018-09-29 13:31:21 -0500

    changed repo to extend jpaRepo rather then crudRepo

--- groodle-server/src/main/java/edu/uno/cs/csci6363/groodleserver/repositories/CourseRepository.java
+++ groodle-server/src/main/java/edu/uno/cs/csci6363/groodleserver/repositories/CourseRepository.java
@@ -1,10 +1,11 @@
 package edu.uno.cs.csci6363.groodleserver.repositories;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import edu.uno.cs.csci6363.groodleserver.entities.Course;
 
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
     List<Course> findAll();
     
