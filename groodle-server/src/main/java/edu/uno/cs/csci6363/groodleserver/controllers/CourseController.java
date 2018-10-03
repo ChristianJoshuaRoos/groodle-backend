2054b5a38477cb7b26c82dc5ea6fd165cd937f34
Christian Roos <cjroos@uno.edu>
2018-10-03 14:35:00 -0500

    Added @CrossOrigin Annotation to CourseController

--- groodle-server/src/main/java/edu/uno/cs/csci6363/groodleserver/controllers/CourseController.java
+++ groodle-server/src/main/java/edu/uno/cs/csci6363/groodleserver/controllers/CourseController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import edu.uno.cs.csci6363.groodleserver.entities.Course;
 import edu.uno.cs.csci6363.groodleserver.services.CourseService;
@@ -17,6 +18,11 @@ public class CourseController {
   @Autowired
   public CourseService courseService;
 
+/*Please add the @CrossOrigin annotation to your code in the 
+back-end.  From what I understand, it allows the front-end server 
+to access the back-end server, through a Web browser.  The front-end
+server is on port 3000.*/
+  @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping("/getCourseList")
   public List<Course>  getCourseList() {
     return courseService.getCourseList();
